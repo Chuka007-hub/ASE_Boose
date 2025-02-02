1d479454a8581866cabb4468109035b5010e4bb4
Chuka007-hub <c.offor2746@student.leedsbeckett.ac.uk>
2025-02-02 17:30:43 +0100

    screenshots

--- Ase_Boose_UnitTest/obj/Debug/net8.0-windows/Ase_Boose_UnitTest.AssemblyInfo.cs
+++ Ase_Boose_UnitTest/obj/Debug/net8.0-windows/Ase_Boose_UnitTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ase_Boose_UnitTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+217926df967d74b7986a69842d186b52e020a8c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1030d4f4da090752ab9c9cb9177f09c5dd6c4891")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ase_Boose_UnitTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Ase_Boose_UnitTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
