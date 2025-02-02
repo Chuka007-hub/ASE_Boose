21ca1b172342d921769b976a821ae2c8ae96161e
Chuka007-hub <c.offor2746@student.leedsbeckett.ac.uk>
2025-02-02 13:30:12 +0100

    Error fix

--- ASE-Boose/Ase-Boose/Ase_Boose_UnitTest/obj/Release/net8.0/Ase_Boose_UnitTest.AssemblyInfo.cs
+++ ASE-Boose/Ase-Boose/Ase_Boose_UnitTest/obj/Release/net8.0/Ase_Boose_UnitTest.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Ase_Boose_UnitTest")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e09db75a22f222f30972d64302fe8ebcdd72887")]
 [assembly: System.Reflection.AssemblyProductAttribute("Ase_Boose_UnitTest")]
