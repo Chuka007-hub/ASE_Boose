336751a6d90b7011ac85e9de0d5ee6bd58d1af45
Chuka007-hub <c.offor2746@student.leedsbeckett.ac.uk>
2025-02-02 10:56:23 +0100

    change folder strucure and add test

---
+++ ASE-Boose/Ase-Boose/Ase_Boose_UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Ase_Boose_UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
